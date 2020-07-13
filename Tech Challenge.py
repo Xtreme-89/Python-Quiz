f392f738bbe2d682f98a6e29980af2dab37593be
Xtreme-89 <mechadave11@gmail.com>
2020-07-13 11:40:55 +0100

    fixed questions, all works (so far)

--- Tech Challenge.py
+++ Tech Challenge.py
@@ -1,7 +1,5 @@
 import ast
-
 import random
-
 from ask_question_func import ask_question
 #to set the value for the score
 score = 0
@@ -10,25 +8,23 @@ score = 0
 print("Would you like to do the easy quiz or the hard quiz?")
 easy_hard = input("\neasy/hard: ")
 # Opens easy and hard files and sets the lists of dictionaries as variables
-with open('hard_questions.txt') as qh:
-    quiz_hard = ast.literal_eval(qh.read())
-
-with open('easy_questions.txt') as qe:
-    quiz_easy = ast.literal_eval(qe.read())
-
 # If the user chose easy, it passes the list of dictionaries in "quiz_easy", same for hard
 if easy_hard.lower() == "easy":
+    with open('easy_questions.txt') as qe:
+        quiz_easy = ast.literal_eval(qe.read())
     print("\n   Welcome to the Quiz of Books!\nThis quiz will be about Harry Potter\n")
     ask_question(quiz_easy, 8)
 # Calls on imported function and chooses quiz and time
 elif easy_hard.lower() == "hard":
+    with open('hard_questions.txt') as qh:
+        quiz_hard = ast.literal_eval(qh.read())
     print("\n     Welcome to the General Knowledge Quiz!\nThis quiz is about random facts about the world\n")
     ask_question(quiz_hard, 4)
 # If user doesn't input hard or easy, it doesn't do the quiz, score 0
 else:
     print("Ok, so you want no quiz.\n")
 # Tells user their score
-print("\nYour Score: " + str(score) + "/10")
+print("\nYour Score: " + str(score) + "/8")
 # Creates a new file
 info = open("info.txt", "w")
 # User inputs name and age
@@ -46,4 +42,4 @@ info.write("Your name: " + name + "\n")
 
 info.write("Your age: " + str(age) + "\n")
 
-info.write("You got: " + str(score) + "/10")
+info.write("You got: " + str(score) + "/8")
